fe5a450d13643d9ff2b1979393a3424445d05fbb
MattTompkins <51715889+MattTompkins@users.noreply.github.com>
2023-11-18 15:49:22 +0000

    Mobile navigation and navar fixes

---
+++ scripts/navigation.js
@@ -0,0 +1,40 @@
+window.addEventListener('scroll', function () {
+	var header = document.querySelector('header');
+	var logo = document.querySelector('.bg-navy');
+	var donateBar = document.querySelector('.bg-red-700');
+	var headerContainer = document.querySelector('#js-header-container');
+
+	if (window.pageYOffset > 0) {
+		header.classList.add('scrolled');
+		header.classList.remove('md:px-5'); // Remove px-5 class when scrolled
+		logo.style.display = 'none';
+		donateBar.style.display = 'none';
+		headerContainer.classList.remove('container', 'max-w-8xl');
+	} else {
+		header.classList.remove('scrolled');
+		header.classList.add('md:px-5'); // Add px-5 class when at the top
+		logo.style.display = 'flex';
+		donateBar.style.display = 'block';
+		headerContainer.classList.add('container', 'max-w-8xl');
+	}
+});
+
+var mobileSidebar = document.getElementById('mobile-sidebar');
+var mobileSidebarButton = document.querySelector('.md\\:hidden button');
+var mobileSidebarOverlay = document.getElementById('mobile-sidebar-overlay');
+
+function toggleMobileSidebar() {
+	mobileSidebar.classList.toggle('translate-x-0');
+	mobileSidebarOverlay.classList.toggle('hidden');
+}
+
+mobileSidebarButton.addEventListener('click', toggleMobileSidebar);
+
+mobileSidebarOverlay.addEventListener('click', toggleMobileSidebar);
+
+window.addEventListener('resize', function () {
+	if (window.innerWidth > 768) {
+		mobileSidebar.classList.remove('translate-x-0');
+		mobileSidebarOverlay.classList.add('hidden');
+	}
+});
